e9ef560c97ddb9488c27556efe39bf5ccf3d3a43
Wolfley-Matt <matt.wolfley@gmail.com>
2016-06-21 20:43:20 -0600

    Update EagleApp.java
    Guys, this is the updated version of the menu

--- BaldEagle/EagleApp/src/eagleapp/EagleApp.java
+++ BaldEagle/EagleApp/src/eagleapp/EagleApp.java
@@ -1,21 +1,67 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package eagleapp;
 
 /**
  *
- * @author Matt
+ * @author Matt Wolfley
  */
-public class EagleApp {
-/**comment test for Brandon
-    /**
-     * @param args the command line arguments
-     */
+public class menu {
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        
+    // Display introduction and menu graphics
+    //Introduction Print
+    System.out.println("=================================");
+    System.out.println("| This application is designed  |");
+    System.out.println("| to help eagle enthusiasts to  |");
+    System.out.println("| track and find eagles through |");
+    System.out.println("| crowd reporting.              |");    
+    System.out.println("=================================");
+    //wait time in milliseconds, we could try building in a "Press Enter to Proceed" option if it's too long lol
+    delay(6000);
+    //Print Menu
+    System.out.println("=================================");
+    System.out.println("|   Eagle App                   |");
+    System.out.println("=================================");
+    System.out.println("| Options:                      |");
+    System.out.println("|        1. Report Eagle        |");
+    System.out.println("|        2. Find Eagle          |");
+    System.out.println("|        3. Exit                |");
+    System.out.println("=================================");
+    System.out.println("Use Number To Make Selection");
+    System.out.println("=================================");
+    Scanner scanner = new Scanner(System.in);
     
+    int choice = scanner.nextInt();
+
+    switch (choice) {
+        case 1:
+            // Perform "Report Eagle" case.
+            break;
+        case 2:
+            // Perform "Find Eagle" case.
+            break;
+        case 3:
+            // Perform "Exit" case.
+            break;
+        default:
+            // Menu Reload.
+    }
+        
+    }
+    //Delay Method
+    private static void delay(int millis) {
+       try {
+            Thread.sleep(millis);
+        } catch (InterruptedException exp) {
+        }
+    
+    }
 }
+
