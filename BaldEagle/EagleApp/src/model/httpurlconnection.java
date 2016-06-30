3501e194fcafc3746d600b635898015a634e72a4
ryancey <rachel14yancey@gmail.com>
2016-06-29 22:57:49 -0600

    unfinished code , but works. It's the general idea of where we are headed with this app.

--- BaldEagle/EagleApp/src/model/httpurlconnection.java
+++ BaldEagle/EagleApp/src/model/httpurlconnection.java
@@ -5,10 +5,31 @@
  */
 package model;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+
 /**
  *
- * @author Rachel Davis
+ * @author Rachel 
  */
 public class httpurlconnection {
-    
+    public static void main(String[] args) throws MalformedURLException, IOException
+    //Created a Java URL object
+    {
+    String urltext = "http://animals.nationalgeographic.com/animals/birds/bald-eagle/";
+    URL url = new URL(urltext);
+    int responseCode = ((HttpURLConnection) url.openConnection()).getResponseCode();
+    if (responseCode == 200 )
+    {
+        System.out.println(responseCode + " " + urltext);
+    }
+    else {
+        System.out.println("Cannot connect");
+    }      
+    }
+
 }
