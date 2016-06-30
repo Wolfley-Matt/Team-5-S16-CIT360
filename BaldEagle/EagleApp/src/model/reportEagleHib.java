82b5f14e62ab607e677e298b69f0db38b51a0839
Lewis-Stevan <stevanlewis1@gmail.com>
2016-06-30 10:47:15 -0600

    Update reportEagleHib.java

--- BaldEagle/EagleApp/src/model/reportEagleHib.java
+++ BaldEagle/EagleApp/src/model/reportEagleHib.java
@@ -1,1 +1,28 @@
+public class reportEagleHib {
+	private int id;
+	private String stateCounties;
 
+	public reportEagleHib() {
+	}
+
+	public reportEagleHib(String scount) {
+		this.stateCounties = scount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStateCounties() {
+		return stateCounties;
+	}
+
+	public void setStateCounties(String state_counties) {
+		this.stateCounties = state_counties;
+	}
+
+}
