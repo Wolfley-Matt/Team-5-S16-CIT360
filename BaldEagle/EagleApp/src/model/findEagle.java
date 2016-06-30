9feef03888a630332e65b0f0fa0d4d612334f106
Lewis-Stevan <stevanlewis1@gmail.com>
2016-06-30 10:40:54 -0600

    Create findEagle.java

---
+++ BaldEagle/EagleApp/src/model/findEagle.java
@@ -0,0 +1,28 @@
+public class findEagle {
+	private int id;
+	private String stateCounties;
+
+	public findEagle() {
+	}
+
+	public findEagle(String scount) {
+		this.stateCounties = scount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStateCounties() {
+		return stateCounties;
+	}
+
+	public void setStateCounties(String state_counties) {
+		this.stateCounties = state_counties;
+	}
+
+}
