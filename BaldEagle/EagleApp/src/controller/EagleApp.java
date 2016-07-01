99159d7f101fe405f902b9ca6abee49721fd9bc6
Davis-Danny <ddude1282@gmail.com>
2016-06-30 18:54:35 -0600

    add loop to main menu

--- BaldEagle/EagleApp/src/controller/EagleApp.java
+++ BaldEagle/EagleApp/src/controller/EagleApp.java
@@ -13,56 +13,62 @@ import java.util.Scanner;
  * @author Matt Wolfley & Danny Davis
  */
 public class EagleApp {
-    public static void main(String[] args) {
-        
-        
-    // Display introduction and menu graphics
-    //Introduction Print
-    System.out.println("=================================");
-    System.out.println("| This application is designed  |");
-    System.out.println("| to help eagle enthusiasts to  |");
-    System.out.println("| track and find eagles through |");
-    System.out.println("| crowd reporting.              |");    
-    System.out.println("=================================");
-    //wait time in milliseconds, we could try building in a "Press Enter to Proceed" option if it's too long lol
-    delay(6000);
-    //Print Menu
-    System.out.println("=================================");
-    System.out.println("|   Eagle App                   |");
-    System.out.println("=================================");
-    System.out.println("| Options:                      |");
-    System.out.println("|        1. Report Eagle        |");
-    System.out.println("|        2. Find Eagle          |");
-    System.out.println("|        3. Exit                |");
-    System.out.println("=================================");
-    System.out.println("Use Number To Make Selection");
-    System.out.println("=================================");
-    Scanner scanner = new Scanner(System.in);
-    
-    int choice = scanner.nextInt();
+	public static void main(String[] args) {
+		boolean repeat = true;
+		// Display introduction and menu graphics
+		// Introduction Print
+		System.out.println("=================================");
+		System.out.println("| This application is designed  |");
+		System.out.println("| to help eagle enthusiasts to  |");
+		System.out.println("| track and find eagles through |");
+		System.out.println("| crowd reporting.              |");
+		System.out.println("=================================");
+		// wait time in milliseconds, we could try building in a "Press Enter to
+		// Proceed" option if it's too long lol
+		delay(6000);
 
-    switch (choice) {
-        case 1:
-            view.reportEagle.report();
-            break;
-        case 2:
-            // Perform "Find Eagle" case.
-            break;
-        case 3:
-            // Perform "Exit" case.
-            break;
-        default:
-            // Menu Reload.
-    }
-        
-    }
-    //Delay Method
-    private static void delay(int millis) {
-       try {
-            Thread.sleep(millis);
-        } catch (InterruptedException exp) {
-        }
-    
-    }
+		do {
+			// Print Menu
+			System.out.println("=================================");
+			System.out.println("|   Eagle App                   |");
+			System.out.println("=================================");
+			System.out.println("| Options:                      |");
+			System.out.println("|        1. Report Eagle        |");
+			System.out.println("|        2. Find Eagle          |");
+			System.out.println("|        3. Exit                |");
+			System.out.println("=================================");
+			System.out.println("Use Number To Make Selection");
+			System.out.println("=================================");
+			Scanner scanner = new Scanner(System.in);
+
+			int choice = scanner.nextInt();
+
+			switch (choice) {
+			case 1:
+				view.reportEagle.report();
+				break;
+			case 2:
+				// Perform "Find Eagle" case.
+				break;
+			case 3:
+				// Perform "Exit" case.
+				repeat = false;
+				break;
+			default:
+				// Menu Reload.
+				System.out.println("Please enter a valid number.");
+			}
+		} while (repeat);
+
+	}
+
+	// Delay Method
+	private static void delay(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException exp) {
+		}
+
+	}
+
 }
-
