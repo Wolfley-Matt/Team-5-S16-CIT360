e5ddb15fa576d6aca1dd4bbd9ca0ac961cbe2e86
Danny Davis <Danny Davis>
2016-07-02 22:25:49 -0600

    added httpurlconnection examples

---
+++ DannyDavis/citbyui/cit360/davisdanny/httpurlconnection/Starter.java
@@ -0,0 +1,10 @@
+package citbyui.cit360.davisdanny.httpurlconnection;
+
+public class Starter {
+
+	public static void main(String[] args) throws Exception {
+		String[] output = {"http://localhost:81/test.html","test"};
+		Reverse.main(output);
+	}
+
+}
