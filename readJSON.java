ee556d72463f2269b7724faecaad68d29f1f4abf
yancey-rachel <yancey-rachel@users.noreply.github.com>
2016-07-02 18:43:38 -0600

    created json file and a readjson java file

---
+++ readJSON.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*package model;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author rachelangilau
+ */
+/*public class readJSON {
+    public static void main(String[]args){
+    JSONparser parser = new JSONparser();
+    
+        try{
+        Object obj = parser.parse(new FileReader(jsonreporteagle.json));
+        
+        JSONObject jsonObject = (JSONObject) obj;
+        JSONArray eagletype = (JSONArray) jsonObject.get("Type of Eagles");
+        
+        System.out.println("\nType of Eagles:");
+        
+        }catch(FileNotFoundException | JSONException e){
+        }
+        
+    }
+
+    private static class JSONparser {
+
+        public JSONparser() {
+        }
+
+        private Object parse(FileReader fileReader) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
+    private static class jsonreporteagle {
+
+        private static String json;
+
+        public jsonreporteagle() {
+        }
+    }
+}*/
