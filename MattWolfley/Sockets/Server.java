6beaa1c017f7f340c4dcaad3969464c140e667f5
Wolfley-Matt <matt.wolfley@gmail.com>
2016-07-12 21:10:16 -0600

    Update Server.java

--- MattWolfley/Sockets/Server.java
+++ MattWolfley/Sockets/Server.java
@@ -5,17 +5,23 @@ import java.net.*;
 
 public class Server {
     public static void main(String args[]) throws Exception {
-        String clientSentence;
-        String lowercaseSentence;
+        //local variables
+        String clientInput;
+        String lowercase;
+        //new socket reception
         ServerSocket welcomeSocket = new ServerSocket(6789);
-
+        //while loop to process any client requests
         while(true) {
+            //while socket reuqest exists
             Socket connectionSocket = welcomeSocket.accept();
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            lowercaseSentence = clientSentence.toLowerCase() + '\n';
-            outToClient.writeBytes(lowercaseSentence);
+            //get client input from socket
+            clientInput = inFromClient.readLine();
+            //actual method to make all letters lowercase
+            lowercase = clientInput.toLowerCase() + '\n';
+            //sent lowercase back to client
+            outToClient.writeBytes(lowercase);
         }
     }
 }
