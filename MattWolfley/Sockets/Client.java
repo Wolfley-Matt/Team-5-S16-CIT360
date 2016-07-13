c223f2ec495acaf828885fea8a0e586d83678b37
Wolfley-Matt <matt.wolfley@gmail.com>
2016-07-12 21:13:27 -0600

    Update Client.java

--- MattWolfley/Sockets/Client.java
+++ MattWolfley/Sockets/Client.java
@@ -1,4 +1,4 @@
-package TCPClient;
+package Client;
 
 import java.io.*;
 import java.net.*;
@@ -6,19 +6,20 @@ import java.net.*;
 public class Client {
     public static void main(String argv[]) throws Exception {
         //take input
-        String sentence;
-        String modifiedSentence;
+        String input;
+        String lowercaseoutput;
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         //Open connection to server
         try ( 
                 Socket clientSocket = new Socket("localhost", 6789)) {
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            //Return sentance and output
-            sentence = inFromUser.readLine();
-            outToServer.writeBytes(sentence + '\n');
-            modifiedSentence = inFromServer.readLine();
-            System.out.println(modifiedSentence);
+            //accept string input and send to server in socket
+            input = inFromUser.readLine();
+            outToServer.writeBytes(input + '\n');
+            //return string and output
+            lowercaseoutput = inFromServer.readLine();
+            System.out.println(lowercaseoutput);
         }
     }
 }
